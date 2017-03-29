585e509296425525f2703958e88418914165955d
yussiv <jussi.voipio@cs.helsinki.fi>
2017-03-29 17:34:11 +0300

    local server setup

--- src/main/java/fi/fubar/bibtex/Main.java
+++ src/main/java/fi/fubar/bibtex/Main.java
@@ -1,8 +1,22 @@
 package fi.fubar.bibtex;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
 public class Main {
-    
-    public static void main(String[] args) {
-        System.out.println("hello fubar");
+
+    @RequestMapping("*")
+    @ResponseBody
+    public String home() {
+        return "Hello fubar!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
     }
 }

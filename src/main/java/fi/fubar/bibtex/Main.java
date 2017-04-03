a9b2ed8b741b88724c37b135f832b1f91767426e
Antiik91 <antiik91@gmail.com>
2017-04-03 11:18:13 +0300

    Added database url into comments to main

--- src/main/java/fi/fubar/bibtex/Main.java
+++ src/main/java/fi/fubar/bibtex/Main.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // Postgres DATABASE_URL: 
+        //postgres://myxhqcqhrmknke:aaba9a491e66671c013575ece44221ff450e3b39de62c74a98300487864c5ac7@ec2-54-225-236-102.compute-1.amazonaws.com:5432/d6dfmrb6fjuq10
+
         SpringApplication.run(Main.class, args);
     }
 }

6b7b790f1cd0c0f01f1e9c784c9af2138d6de3a5
yussiv <jussi.voipio@cs.helsinki.fi>
2017-04-03 12:03:50 +0300

    test folder

---
+++ src/test/java/fi/fubar/bibtext/BookTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.fubar.bibtext;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jkvoipio
+ */
+public class BookTest {
+    
+    public BookTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}

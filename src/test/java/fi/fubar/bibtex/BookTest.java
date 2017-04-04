1eb17f01baae30fda3b4848417443fa86da4cd8e
Saska Dönges <tuomionpupu@gmail.com>
2017-04-04 15:49:08 +0300

    some fixing

--- src/test/java/fi/fubar/bibtex/BookTest.java
+++ src/test/java/fi/fubar/bibtex/BookTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fi.fubar.bibtext;
+package fi.fubar.bibtex;
 
 import org.junit.Before;
 import org.junit.Test;
--- src/test/java/fi/fubar/bibtex/Stepdefs.java
+++ src/test/java/fi/fubar/bibtex/Stepdefs.java
@@ -1,15 +1,17 @@
-package fi.fubar;
+package fi.fubar.bibtex;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import static fi.fubar.RunCukesTest.server;
 import static org.junit.Assert.assertTrue;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Stepdefs {
+    WebDriver driver = new ChromeDriver();
+    
     @Given("^user is logged in$")
     public void user_is_logged_in() throws Throwable {
         logInWith("foo", "bar");
@@ -17,31 +19,29 @@ public class Stepdefs {
 
     @Given("^has navigated to the \"([^\"]*)\" page$")
     public void has_navigated_to_the_page(String arg1) throws Throwable {
-        assertTrue(server.getDriver().getCurrentUrl().contains("references"));
+        assertTrue(driver.getCurrentUrl().contains("references"));
     }
 
     @When("^the entry \"([^\"]*)\" is entered into the field \"([^\"]*)\"$")
     public void the_entry_is_entered_into_the_field(String entry, String field) throws Throwable {
-        WebElement elem = server.getDriver().findElement(By.name(field));
+        WebElement elem = driver.findElement(By.name(field));
         elem.sendKeys(entry);
     }
 
     @When("^the form is submitted$")
     public void the_form_is_submitted() throws Throwable {
-        WebElement elem = server.getDriver().findElement(By.name("submit"));
+        WebElement elem = driver.findElement(By.name("submit"));
         elem.submit();
     }
 
     @Then("^a reference to \"([^\"]*)\" is added\\.$")
     public void a_reference_to_is_added(String ref) throws Throwable {
-        WebDriver driver = server.getDriver();
         assertTrue(driver.getCurrentUrl().contains("references"));
         assertTrue(driver.getPageSource().contains(ref));
     }
 
     private void logInWith(String foo, String bar) throws InterruptedException {
-        WebDriver driver = server.getDriver();
-        driver.get("http://localhost:" + server.getPort());
+        driver.get("http://localhost:" + 8080);
         assertTrue(driver.getPageSource().contains("Login with Username and Password"));
         WebElement element = driver.findElement(By.name("username"));
         element.sendKeys(foo);

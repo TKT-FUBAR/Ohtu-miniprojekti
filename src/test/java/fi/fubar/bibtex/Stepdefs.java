e4fe871ce13b7197ebadc361caefbd8191e2d606
yussiv <jussi.voipio@cs.helsinki.fi>
2017-04-05 14:35:46 +0300

    Selenium version change, refactored tests

--- src/test/java/fi/fubar/bibtex/Stepdefs.java
+++ src/test/java/fi/fubar/bibtex/Stepdefs.java
@@ -3,32 +3,30 @@ package fi.fubar.bibtex;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-//import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import static org.junit.Assert.assertTrue;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Stepdefs {
+
     WebDriver driver;
 
     public Stepdefs() {
-        //ChromeDriverManager.getInstance().setup();
+        FirefoxDriverManager.getInstance().setup();
         this.driver = new FirefoxDriver();
     }
-    
-    
-    
+
     @Given("^user is logged in$")
     public void user_is_logged_in() throws Throwable {
         logInWith("foo", "bar");
     }
-    
+
     @Given("^user navigates to the \"([^\"]*)\" page$")
     public void user_navigates_to_the_page(String str1) throws Throwable {
-        driver.get("http://localhost:" + 8080 +"/"+str1);
+        driver.get("http://localhost:" + 8080 + "/" + str1);
     }
 
     @Given("^has navigated to the \"([^\"]*)\" page$")
@@ -38,14 +36,12 @@ public class Stepdefs {
 
     @When("^the entry \"([^\"]*)\" is entered into the field \"([^\"]*)\"$")
     public void the_entry_is_entered_into_the_field(String entry, String field) throws Throwable {
-        WebElement elem = driver.findElement(By.name(field));
-        elem.sendKeys(entry);
+        driver.findElement(By.name(field)).sendKeys(entry);
     }
 
     @When("^the form is submitted$")
     public void the_form_is_submitted() throws Throwable {
-        WebElement elem = driver.findElement(By.name("submit"));
-        elem.submit();
+        driver.findElement(By.name("submit")).click();
         Thread.sleep(1000l);
     }
 
@@ -54,13 +50,13 @@ public class Stepdefs {
         assertTrue(driver.getCurrentUrl().contains("references"));
         assertTrue(driver.getPageSource().contains(ref));
     }
-    
+
     @Then("^a \"([^\"]*)\" is added\\.$")
     public void a_is_added(String arg1) throws Throwable {
         assertTrue(driver.getCurrentUrl().contains("createuser"));
         assertTrue(driver.getPageSource().contains(arg1));
     }
-    
+
     private void logInWith(String foo, String bar) throws InterruptedException {
         driver.get("http://localhost:" + 8080);
         assertTrue(driver.getPageSource().contains("Login with Username and Password"));
@@ -68,7 +64,7 @@ public class Stepdefs {
         element.sendKeys(foo);
         element = driver.findElement(By.name("password"));
         element.sendKeys(bar);
-        element.submit();
+        driver.findElement(By.name("submit")).click();
         Thread.sleep(1000l);
     }
 

58b2c80f94dc6d0162a38a9c17aec40f3930f185
lrkr <lkarvonen@gmail.com>
2017-04-29 20:36:40 +0300

    import now attaches the imported reference to user. import format error handling

--- src/main/java/fi/fubar/bibtex/service/ImportBibTexService.java
+++ src/main/java/fi/fubar/bibtex/service/ImportBibTexService.java
@@ -7,6 +7,7 @@ import fi.fubar.bibtex.domain.Reference;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +15,14 @@ public class ImportBibTexService {
 
     @Autowired
     private ReferenceService referenceService;
+    @Autowired
+    private UserService userService;
 
     public void importBibTex(String bibtex) {
-        System.out.println(bibtex);
         Reference ref = getRefType(bibtex);
         if (ref != null) {
             parseData(ref, bibtex);
+            ref.setOwner(userService.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName()));
             referenceService.save(ref);
         }
     }
@@ -27,26 +30,32 @@ public class ImportBibTexService {
     private Reference getRefType(String data) {
         Pattern p = Pattern.compile("(?<=\\@)(.*?)(?=\\{)");
         Matcher m = p.matcher(data);
-        m.find();
-        String type = m.group(1).toLowerCase();
-        switch (type) {
-            case "book":
-                return new Book();
-            case "article":
-                return new Article();
-            case "inproceedings":
-                return new InProceedings();
-            default:
-                return null;
+        if (m.find()) {
+            String type = m.group(1).toLowerCase();
+            switch (type) {
+                case "book":
+                    return new Book();
+                case "article":
+                    return new Article();
+                case "inproceedings":
+                    return new InProceedings();
+                default:
+                    return null;
+            }
         }
+        return null;
     }
 
     private void parseData(Reference ref, String data) {
         String[] split = data.split("\n");
         //parse fields that are in all references        
         ref.setHandle(parseHandle(split[0]));
-        ref.setTitle(parseField("title", split));
-        ref.setYear(parseField("year", split));        
+        String title = parseField("title", split);
+        if (title.isEmpty()) {
+            title = "default";
+        }
+        ref.setTitle(title);
+        ref.setYear(parseField("year", split));
         ref.setAuthor(parseField("author", split));
         ref.setNumber(parseField("number", split));
         ref.setVolume(parseField("volume", split));
@@ -87,22 +96,25 @@ public class ImportBibTexService {
     }
 
     private String parseField(String field, String[] data) {
-        String ret = "";
         for (int i = 0; i < data.length; i++) {
             if (data[i].contains(field + " = {")) {
                 Pattern p = Pattern.compile("(?<=\\{)(.*?)(?=\\})");
                 Matcher m = p.matcher(data[i]);
-                m.find();
-                return m.group(1);
+                if (m.find()) {
+                    return m.group(1);
+                }
             }
         }
-        return ret;
+        return "";
     }
 
     private String parseHandle(String handle) {
         Pattern p = Pattern.compile("(?<=\\{)(.*?)(?=,)");
         Matcher m = p.matcher(handle);
-        m.find();
-        return m.group(1);
+        if (m.find()) {
+            return m.group(1);
+        } else {
+            return "default";
+        }
     }
 }

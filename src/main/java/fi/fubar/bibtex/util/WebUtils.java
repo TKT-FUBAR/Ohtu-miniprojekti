27ddd83a2084394a7e1da19d7fddfe04947e749d
yussiv <jussi.voipio@cs.helsinki.fi>
2017-05-03 14:50:18 +0300

    Regexp correction

--- src/main/java/fi/fubar/bibtex/util/WebUtils.java
+++ src/main/java/fi/fubar/bibtex/util/WebUtils.java
@@ -13,22 +13,28 @@ public class WebUtils {
 
     public static String getACMData(String url) {
         String id = parseId(url);
-        if (id == null) return null;        
+        if (id == null) {
+            return null;
+        }
         try {
             Document doc = Jsoup.connect("http://dl.acm.org/exportformats.cfm?id=" + id + "&expformat=bibtex").get();
             String data = doc.select("PRE").first().ownText();
             return StringUtils.scandicsBack(data);
-        } catch (IOException ex) {
-            System.out.println(ex);
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
             return null;
         }
     }
 
     private static String parseId(String url) {
-        Pattern p = Pattern.compile("(?<=\\?id=)(\\d*)");
+        Pattern p = Pattern.compile("(\\?|&)id=([^&]+)");
         Matcher m = p.matcher(url);
         if (m.find()) {
-            return m.group(1);
+            if (m.group(2).contains(".")) {
+                return m.group(2).substring(m.group(2).indexOf(".") + 1);
+            } else {
+                return m.group(2);
+            }
         } else {
             return null;
         }
